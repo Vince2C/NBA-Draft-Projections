c41206424516ef9ed5947d3da5dd29dab4db7cb2
Vince <vincech@gmail.com>
2021-12-15 00:16:04 -0500

    updated mainpage

--- src/mainpage.js
+++ src/mainpage.js
@@ -2,9 +2,23 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import image from "./dametime.png";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {AgGridColumn, AgGridReact} from 'ag-grid-react';
+import axios from 'axios';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 export default function Mainpage() {
   const navigate = useNavigate();
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const result = await axios("../api");
+  //     setData(result.data);
+  //   })();
+  // }, []);
 
   return (
     <div>
